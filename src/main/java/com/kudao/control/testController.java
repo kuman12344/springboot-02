475ad1071699048050430a25567335a45f007899
cxz <hello@cxz.com>
2023-03-29 16:44:25 +0800

    新增的controller

---
+++ src/main/java/com/kudao/control/testController.java
@@ -0,0 +1,16 @@
+package com.kudao.control;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+
+    @GetMapping("/test")
+    public Object test(){
+
+        return "github 测试成功!";
+
+    }
+
+}
